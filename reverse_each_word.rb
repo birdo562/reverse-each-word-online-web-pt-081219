cbcb853bbf3ed5b3702cc95b3f31f7b1ecc76e3a
Roberto Medina <robby.medina562@gmail.com>
2019-08-20 20:57:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence_array)
   og_array= sentence_array.split(", ")
   new_array = []
-  new_array.each {|string|new_array << og_array.reverse}
+  new_array
   return new_array.join("")
 end
